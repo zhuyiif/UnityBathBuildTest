4c2e1ba739016772cba2ab429a478de7a25d93e5
zack zhu <36757049@qq.com>
2018-08-31 12:17:01 -0700

    init

---
+++ Assets/Editor/TestBuild.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TestBuild {
+
+    static void PerformBuild ()
+    {
+
+        Debug.Log ("PerformBuild 2");
+       
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions ();
+        buildPlayerOptions.scenes = new [] { "Assets/Scenes/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "LinuxBuild";
+        buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
+        buildPlayerOptions.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer (buildPlayerOptions);
+
+    }
+}
